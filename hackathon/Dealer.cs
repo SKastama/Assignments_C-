efd31fb0eecb6a17e060f3dde1586414993ca8a0
Sarah Kastama <sarahkastama@gmail.com>
2021-09-29 15:21:51 -0700

    commit

---
+++ hackathon/Dealer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace deck_of_cards
+{
+    class Dealer : Player
+    {
+        public Card ShownCard;
+        public Card HiddenCard;
+        public Dealer(Deck deck) : base(deck)
+        {
+            this.name = "Cody";
+        }
+        public override Card Hit() 
+        {
+            Card card = null;
+            if (this.points >= 17) {
+                this.Stand();
+                return card;
+            } else {
+                card = this.Draw();
+            }
+            if (this.points > 21) {
+                for (var i=0; i<this.Hand.Count; i++) {
+                    if (this.Hand[i].Val == 11) {
+                        this.Hand[i].Val = 1;
+                        this.points -= 10;
+                        break;
+                    }
+                }
+                this.lose = true;
+                this.play = false;
+            }
+            return card;
+        } 
+        public override void Stand()
+        {
+            this.play = false;
+        }
+    }
+}
